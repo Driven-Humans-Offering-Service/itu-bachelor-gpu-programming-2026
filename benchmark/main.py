26a4462349a69d515cc0502f1110859bc6072c2c
= <daniel@haugboelle.dk>
2026-01-28 12:46:07 +0100

    format file

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,26 +1,33 @@
-import random as rand
-from pathlib import Path
 import argparse
+import random as rand
 
 
 def generateMatrix(n, num):
     with open(f"../matrices/matrix_{num}_{n}x{n}.csv", "w") as f:
         f.write(f"{n}\n")
-        for i in range(0,n):
-            for j in range(0,n):
-                f.write(f"{rand.uniform(0,10)},")
+        for i in range(0, n):
+            for j in range(0, n):
+                f.write(f"{rand.uniform(0, 10)},")
             f.write("\n")
 
+
 def setupArguments():
-    parser = argparse.ArgumentParser(description='Benchmarking tool for bachelor')
-    _ = parser.add_argument("--genMatricies", "-gm", dest="amountOfMatricies", type=int, default=-1, help="Amount of matricies to create, every new one doubles in size")
+    parser = argparse.ArgumentParser(description="Benchmarking tool for bachelor")
+    _ = parser.add_argument(
+        "--genMatricies",
+        "-gm",
+        dest="amountOfMatricies",
+        type=int,
+        default=-1,
+        help="Amount of matricies to create, every new one doubles in size",
+    )
 
     return parser.parse_args()
 
-def main():
 
+def main():
     args = setupArguments()
-    
+
     if args.amountOfMatricies != -1:
         print("generating matrices")
         rand.seed("Driven Humans Offering Service")
@@ -28,11 +35,9 @@ def main():
         for _ in range(0, args.amountOfMatricies):
             generateMatrix(i, 0)
             generateMatrix(i, 1)
-            i*=2
+            i *= 2
     print("done!")
 
 
 if __name__ == "__main__":
     main()
-
-            
