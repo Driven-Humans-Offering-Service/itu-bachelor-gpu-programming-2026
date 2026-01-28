3ce1913d10961b0e3cb23113eccd0b773ba6f191
= <=>
2026-01-28 10:29:27 +0100

    better generator

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,18 +1,23 @@
 import random as rand
+from pathlib import Path
+
 
 def generateMatrix(n):
-    with open(f"matrix_{n}x{n}.csv", "w") as f:
+    with open(f"../matrices/matrix_{n}x{n}.csv", "w") as f:
         for i in range(0,n):
             for j in range(0,n):
                 f.write(f"{rand.uniform(0,10)},")
             f.write("\n")
 
 def main():
-    i = 20
-    for _ in range(0,8):
-        generateMatrix(i)
-        i*=2
-    print("Hello from benchmark!")
+    if not Path("../matrices/matrix_20x20.csv").exists():
+        print("generating matrices")
+        rand.seed("Driven Humans Offering Service")
+        i = 20
+        for _ in range(0,8):
+            generateMatrix(i)
+            i*=2
+    print("done!")
 
 
 if __name__ == "__main__":
