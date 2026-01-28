27862ad5d78ba1b41eaddd8d55ea609e1ce547a6
= <=>
2026-01-28 10:17:40 +0100

    generator for matrices

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,6 +1,21 @@
+import random as rand
+
+def generateMatrix(n):
+    with open(f"matrix_{n}x{n}.csv", "w") as f:
+        for i in range(0,n):
+            for j in range(0,n):
+                f.write(f"{rand.uniform(0,10)},")
+            f.write("\n")
+
 def main():
+    i = 20
+    for _ in range(0,8):
+        generateMatrix(i)
+        i*=2
     print("Hello from benchmark!")
 
 
 if __name__ == "__main__":
     main()
+
+            
