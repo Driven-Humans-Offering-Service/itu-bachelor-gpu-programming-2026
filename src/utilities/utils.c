927665fe9cb334587d21acd09e8a28155b0b4768
Oliver Starup <Oliver@starup99.dk>
2026-02-04 14:20:20 +0100

    Fixed string compare

--- src/utilities/utils.c
+++ src/utilities/utils.c
@@ -1,7 +1,7 @@
 #include <string.h>
 int contains_argument(int argc, char** argv, const char* arg){
     for (int i = 0; i < argc; i++) {
-        if(strcmp(argv[i], arg)) return 1;
+        if(strcmp(argv[i], arg) == 0) return 1;
     }
     return 0;
 }
