09ac06de9b4c2da0404a88204e2f9148442905bc
= <daniel@haugboelle.dk>
2026-02-04 09:55:48 +0100

    refactor to matrix util

---
+++ addition/matrix_addition.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../utilities/matrix.h"
+
+void addMatrices(float* res, float* m1, float* m2, int size);
+
+struct timespec tid;
+unsigned long runtime;
+
+int main(int argc, char** argv) {
+    char* path1 = argv[argc - 2];
+    char* path2 = argv[argc - 1];
+
+    Matrices* ma = load_matrices(path1, path2);
+
+    int total_size = ma->total_size;
+    //printMatrix(matrix1, size);
+    //printMatrix(matrix2, size);
+    float* result = malloc(sizeof(float) * total_size);
+    addMatrices(result, ma->m1, ma->m2, total_size);
+    //printMatrix(result, size);
+    free_matrices(ma);
+    free(result);
+
+    printf("The runtime was: %lu ns\n", runtime);
+    return 0;
+}
+
+void add_matrices(float* res, float* m1, float* m2, int size) {
+    clock_gettime(CLOCK_REALTIME,&tid);
+    unsigned long before = tid.tv_nsec;
+    for (int i = 0; i < size; i++) {
+        res[i] = m1[i] + m2[i];
+    }
+    clock_gettime(CLOCK_REALTIME,&tid);
+    unsigned long after = tid.tv_nsec;
+    runtime = after - before;
+}
---
+++ utilities/matrix.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct Matrices {
+    int total_size;
+    float* m1;
+    float* m2;
+} Matrices;
+
+void free_matrices(Matrices* ma);
+
+Matrices* init_matrices(int size);
+
+void read_matrix(FILE* file, float* m, int size);
+
+void print_matrix(float* m, int size);
+
+Matrices* load_matrices(char* filename1, char* filename2);
