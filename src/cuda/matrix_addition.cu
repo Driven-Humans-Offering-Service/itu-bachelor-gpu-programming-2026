7553f237cb8ca838c3c21b65108428a18632b8ae
Oliver Starup <Oliver@starup99.dk>
2026-02-04 12:47:33 +0100

    Removed unneed stuff

--- src/cuda/matrix_addition.cu
+++ src/cuda/matrix_addition.cu
@@ -8,67 +8,6 @@
 #include <driver_types.h>
 #include <stdlib.h>
 
-Matrices *load_matrices(char *filename1, char *filename2) {
-
-  FILE *file1 = fopen(filename1, "r");
-  FILE *file2 = fopen(filename2, "r");
-
-  char *error_message = "";
-  if (!file1) {
-    sprintf(error_message, "No file with that name %s\n", filename1);
-    printf("%s", error_message);
-    exit(1);
-  } else if (!file2) {
-    sprintf(error_message, "No file with that name %s\n", filename2);
-    printf("%s", error_message);
-    exit(1);
-  }
-
-  int size = 0;
-  fscanf(file1, "%d", &size);
-  int totalsize = size * size;
-  Matrices *ma = init_matrices(totalsize);
-  read_matrix(file1, ma->m1, totalsize);
-  fscanf(file2, "%d", &size);
-  read_matrix(file2, ma->m2, totalsize);
-
-  return ma;
-}
-
-void read_matrix(FILE *file, float *m, int size) {
-  for (int i = 0; i < size; i++) {
-    float x = 2;
-    fscanf(file, "%f, ", &x);
-    m[i] = x;
-  }
-}
-
-void print_matrix(float *m, int size) {
-  for (int i = 0; i < size; i++) {
-    printf("[ ");
-    for (int j = 0; j < size; j++) {
-      printf("%f, ", m[i * size + j]);
-    }
-    printf("]\n");
-  }
-}
-
-Matrices *init_matrices(int total_size) {
-  Matrices *ma = (Matrices *)malloc(sizeof(Matrices));
-  ma->total_size = total_size;
-  ma->m1 = (float *)malloc(sizeof(float) * total_size);
-  ma->m2 = (float *)malloc(sizeof(float) * total_size);
-  ma->result = (float *)malloc(sizeof(float) * total_size);
-  return ma;
-}
-
-void free_matrices(Matrices *ma) {
-  free(ma->m1);
-  free(ma->m2);
-  free(ma->result);
-  free(ma);
-}
-
 // Taken from
 // https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
 #define gpuErrchk(ans)                                                         \
