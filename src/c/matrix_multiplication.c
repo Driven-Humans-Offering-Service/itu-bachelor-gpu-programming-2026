736722b0bdc5e4363e5d175281bfaa90508c4764
Oliver Starup <Oliver@starup99.dk>
2026-02-11 09:18:34 +0100

    Implemented naive multiply

---
+++ src/c/matrix_multiplication.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "../utilities/matrix.h"
+#include "../utilities/utils.h"
+
+#define IDX(i,j,size) (i * size + j)
+
+void multiply_matrices(float* res, float* m1, float* m2, int size);
+
+
+int main(int argc, char** argv) {
+    char* path1 = argv[argc - 2];
+    char* path2 = argv[argc - 1];
+    int displayRuntime = contains_argument(argc, argv, "--time");
+    int print_to_file = contains_argument(argc, argv, "--outputresult");
+
+    Matrices* ma = load_matrices(path1, path2);
+
+    int total_size = ma->total_size;
+    //printMatrix(matrix1, size);
+    //printMatrix(matrix2, size);
+    unsigned long before = get_time_nanoseconds();
+    multiply_matrices(ma->result, ma->m1, ma->m2, ma->size);
+    unsigned long after = get_time_nanoseconds();
+    unsigned long runtime = after - before;
+    //printMatrix(result, size);
+
+    if(displayRuntime) 
+        printf("%lu\n", runtime);
+
+    if(print_to_file){
+        char* path = argv[print_to_file+1];
+        output_matrix(ma, path);
+    }
+    free_matrices(ma);
+    return 0;
+}
+
+void multiply_matrices(float* res, float* m1, float* m2, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < size; k++) {
+            for (int j = 0; j < size; j++) {
+                res[IDX(i, j, size)] = m1[IDX(i,k,size)] + m2[IDX(k,j,size)];
+            }
+        }
+    }
+}
