f95862e65be400d76cca511338b358176f24fa97
Oliver Starup <Oliver@starup99.dk>
2026-02-18 09:32:59 +0100

    Made java inversion work

--- src/java/MatrixInversion.java
+++ src/java/MatrixInversion.java
@@ -5,7 +5,8 @@ public class MatrixInversion {
 
     public static int hasArgument(String[] args, String arg) {
         for (int i = 0; i < args.length; i++) {
-            if (args[i].equals(arg)) return i;
+            if (args[i].equals(arg))
+                return i;
         }
         return -1;
     }
@@ -58,6 +59,71 @@ public class MatrixInversion {
         }
     }
 
+    public static float[] findY(float[][] alpha, float[] b) {
+        int N = alpha.length;
+        float[] y = new float[N];
+
+        y[0] = b[0] / alpha[0][0];
+
+        for (int i = 1; i < N; i++) {
+            float sum = 0.0f;
+            for (int j = 0; j < i; j++) {
+                sum += alpha[i][j] * y[j];
+            }
+            y[i] = (b[i] - sum) / alpha[i][i];
+        }
+
+        return y;
+    }
+
+    public static float[] findX(float[][] beta, float[] y) {
+        int N = beta.length;
+        float[] x = new float[N];
+
+        x[N - 1] = y[N - 1] / beta[N - 1][N - 1];
+
+        for (int i = N - 2; i >= 0; i--) {
+
+            float sum = 0.0f;
+
+            for (int j = i; j < N; j++) {
+                sum += beta[i][j] * x[j];
+            }
+
+            x[i] = (y[i] - sum) / beta[i][i];
+        }
+
+        return x;
+
+    }
+
+    public static float[][] getIdentityMatrix(int N) {
+        float[][] E = new float[N][N];
+
+        for (int i = 0; i < N; i++) {
+            E[i][i] = 1;
+        }
+        return E;
+    }
+
+    public static float[][] inverse(float[][] alpha, float[][] beta) {
+        int N = alpha.length;
+
+        float[][] E = getIdentityMatrix(N);
+
+        float[][] X = new float[N][N];
+
+        for (int i = 0; i < N; i++) {
+            float[] y = findY(alpha, E[i]);
+            X[i] = findX(beta, y);
+        }
+
+        transposeMatrix(X);
+
+        return X;
+
+    }
+
     public static void main(String[] args) throws IOException {
         Matrix matrix = new Matrix(args[args.length - 2]);
 
@@ -65,6 +131,7 @@ public class MatrixInversion {
         Matrix[] matrices = getLUDecomposition(matrix.m);
         Matrix l = matrices[0];
         Matrix u = matrices[1];
+        float[][] X = inverse(l.m, u.m);
         long after = System.nanoTime();
         long runtime = after - before;
 
@@ -74,10 +141,11 @@ public class MatrixInversion {
         if (Arrays.stream(args).anyMatch("--printResult"::equals)) {
             Matrix.printMatrix(l.m);
             Matrix.printMatrix(u.m);
+            Matrix.printMatrix(X);
         }
         // int outputResult = hasArgument(args, "--outputresult");
         // if (outputResult > -1) {
-        //     Matrix.outputMatrix(result, args[outputResult + 1]);
+        // Matrix.outputMatrix(result, args[outputResult + 1]);
         // }
     }
 }
