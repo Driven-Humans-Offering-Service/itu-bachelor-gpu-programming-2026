b2a3f141d14e3c5352112cd94c11a56c19333201
Oliver Starup <Oliver@starup99.dk>
2026-02-20 13:37:52 +0100

    Debug

--- src/cuda/matrix_inversion.cu
+++ src/cuda/matrix_inversion.cu
@@ -98,6 +98,8 @@ __global__ void findx(float *alpha, float *beta, float *b_full, float *x_full,
                       float *y_full, const int N) {
 
   int col = blockDim.x * blockIdx.x + threadIdx.x;
+  *y_full = 1;
+  return;
 
   if (col >= N)
     return;
