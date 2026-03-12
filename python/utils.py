41db31f633c62e09e8d28c0a95967f7bbffcaf09
Oliver Starup <Oliver@starup99.dk>
2026-03-12 15:05:10 +0100

    New benchmark works

--- python/utils.py
+++ python/utils.py
@@ -35,10 +35,38 @@ def get_files_for_lang_arg(lang):
     if lang == "all":
         for tp in ["java","cuda","c"]:
             build_folder = os.path.join(rootFolder, f"./build/{tp}")
-            files = get_files_containg(build_folder, type)
+            files = get_files_containg(build_folder, "")
     else:
         build_folder = os.path.join(rootFolder, f"./build/{lang}")
-        files = get_files_containg(build_folder, type)
+        files = get_files_containg(build_folder, "")
     return files
+
+def filter_files_by_operation(files: list[str], op: str):
+    op = op.lower()
+    if op == "all":
+        return files
+    return list(filter(lambda file: op in file.lower(), files))
+
+def filter_files_by_iteration(files: list[str], iterations: int):
+    m: dict[str, int] = {}
+    ending = {}
+    new_files: list[str] = []
+    for file in files:
+        split = file.split("_")
+        file_name = "_".join(split[:-1])
+        m[file_name] = int(split[-1].split(".")[0])
+        ending[file_name] = "." + split[-1].split(".")[1]
+    for file in m:
+        for i in range(0, iterations):
+            num = m[file] - i
+            if num < 0:
+                break
+            new_files.append(file + "_" + str(num) + ending[file])
+    return new_files
+
+
+
+
+        
 
 setup()
