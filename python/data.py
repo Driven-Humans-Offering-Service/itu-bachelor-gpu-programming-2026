22680dba5161080255ce3168d1328b14b4b7f8b7
iiinaD <daniel@haugboelle.dk>
2026-03-19 12:15:30 +0100

    improve data.py

--- python/data.py
+++ python/data.py
@@ -1,13 +1,18 @@
 
 class BenchmarkData:
-    def __init__(self, hardware, size, iteration, algorithm, language):
+    def __init__(self, hardware, iteration, algorithm, language):
         self.hardware = hardware
-        self.size = size
         self.iteration = iteration
         self.algorithm = algorithm
         self.language = language
         self.x = []
         self.y = []
 
-    def __hash__(self):
-        return hash(self.hardware + self.size + self.iteration + self.algorithm + self.language)
+    def description(self):
+        return self.hardware + self.iteration + self.algorithm + self.language
+
+    def appendx(self, x):
+        self.x.append(x)
+
+    def appendy(self, y):
+        self.y.append(y)
